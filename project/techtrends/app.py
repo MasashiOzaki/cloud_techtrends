65af55baedafc1854136e195a30e1fe8e6391c4c
Masashi Ozaki <masashi.ozaki.te@renesas.com>
2024-08-05 22:55:07 +0900

    step1: add metric: db_connection_count

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -3,11 +3,16 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+# Global variable to keep track of the number of database connections
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1  # Increment the connection count
     return connection
 
 # Function to get a post using its ID
@@ -72,6 +77,19 @@ def status():
         response=json.dumps({"result":"OK - healthy"}),
         status=200,
         mimetype="application/json"
+    )
+    return response
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
+    connection.close()
+    
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": post_count}),
+            status=200,
+            mimetype='application/json'
     )
     return response
 
